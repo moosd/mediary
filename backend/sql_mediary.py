d07e01a712f5d0232c9de627736201e5ef64a3f1
Soham-G <sohamgarg@hotmail.co.uk>
2019-01-20 10:11:20 +0000

    flask app for sql database

--- backend/sql_mediary.py
+++ backend/sql_mediary.py
@@ -3,7 +3,8 @@ import pandas as pd
 
 import os
 from flask import Flask, flash, request, redirect, url_for
-from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
 
 
 server = 'mediary.database.windows.net'
@@ -11,29 +12,52 @@ database = 'MeDiary'
 username = 'mediary'
 password = 'hackcambridge-1'
 driver= '{ODBC Driver 17 for SQL Server}'
+
 cnxn = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
 cursor = cnxn.cursor()
 
-app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+@app.route('/', methods=['GET', 'POST'])
+def upload_data():
+    # app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-# cursor.execute("SELECT @@version;")
-# row = cursor.fetchone()
-# while row:
-#     print(row[0])
-#     row = cursor.fetchone()
+    nhs_number = request.args.get("nhs_number")
+    condition = request.args.get("condition")
 
-# View database
-query = "SELECT * FROM Conditions"
-df = pd.read_sql(query, cnxn)
-print(df)
+    # View database
+    query = "SELECT * FROM Conditions where nhs_number=" + nhs_number
+    df = pd.read_sql(query, cnxn)
 
-# Write to database
-cursor.execute("INSERT INTO Conditions(nhs_number, condition,img1, img2) values ('01234', 'condition', 'img1', 'img2')")
-cnxn.commit()
+    df.head()
+    print(df)
+    # print(df)
+    # Write to database
+    cursor.execute("INSERT INTO Conditions(nhs_number, condition) values (" + nhs_number + ", '" + condition + "') ")
+    cnxn.commit()
+
+    return '''
+    	    <!doctype html>
+    	    <title>File Link</title>
+    	    <h1>Uploaded File Link</h1>
+    	    '''
+    return '''
+        <!doctype html>
+        <title>Upload new File</title>
+        <h1>Upload new File</h1>
+        <form action="?nhs_number=0&condition=none" method=post enctype=multipart/form-data>
+          <p><input type=file name=file>
+             <input type=submit value=Upload>
+        </form>
+        '''
+
+
+
+
+
 
 # cursor.execute("DELETE FROM Conditions")
 # cnxn.commit()
+if __name__ == '__main__':
+    app.run(debug=True, port = 5001)
 
-cursor.close()
-cnxn.close()
+    cursor.close()
+    cnxn.close()
